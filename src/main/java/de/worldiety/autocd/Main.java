6e98489fe742418b9e1cfb5cb816970239d927c8
Lucas Wagner <lucas.wagner@worldiety.de>
2019-10-15 11:01:16 +0200

    Documentaion

--- src/main/java/de/worldiety/autocd/Main.java
+++ src/main/java/de/worldiety/autocd/Main.java
@@ -16,15 +16,12 @@ import io.kubernetes.client.custom.V1Patch;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.Config;
 import io.kubernetes.client.util.credentials.AccessTokenAuthentication;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -41,7 +38,7 @@ public class Main {
         String name = "autocd.json";
         var autoCD = getAutoCD(name, true);
         var oldAutoCD = getAutoCD("oldautocd.json", false);
-
+        // A new API Client is created. Docker Credentials will be obtained from the Digital Oceans cluster configuration file
         ApiClient strategicMergePatchClient = ClientBuilder.standard()
                 .setBasePath(args[0])
                 .setVerifyingSsl(true)
@@ -55,7 +52,7 @@ public class Main {
                 System.getenv(Environment.K8S_REGISTRY_USER_NAME.toString()),
                 System.getenv(Environment.K8S_REGISTRY_USER_TOKEN.toString())
         );
-
+        // Determines the build Type
         String buildType;
         if (args.length == 4) {
             buildType = args[BUILD_TYPE];
@@ -65,9 +62,17 @@ public class Main {
 
         DockerfileHandler finder = new DockerfileHandler(".");
 
+
+        //Creat the k8s client, CoreV1Api is needed for the client
         CoreV1Api patchApi = new CoreV1Api(strategicMergePatchClient);
         CoreV1Api api = new CoreV1Api();
         var k8sClient = new K8sClient(api, finder, buildType, patchApi, dockerCredentials);
+
+
+        /* This method checks for amy images which are referred in the main autoCD object
+           If invalid images are found, they will be removed from the cluster.
+           An image is invalid if there is no registry image path containing the name of the targeted image.
+        */
 
         if (oldAutoCD != null) {
             var validImageNames = autoCD.getOtherImages().stream()
@@ -91,6 +96,9 @@ public class Main {
         populateSubdomain(autoCD, buildType, autoCD.getSubdomains());
         populateContainerPort(autoCD, finder);
 
+        /* Checks if the app should be hosted on the cluster, if one decides to abandon the app, this method will
+            remove it if the tag 'isShouldHost' is set correctly (false)
+        */
 
         if (!autoCD.isShouldHost()) {
             if (oldAutoCD != null && oldAutoCD.isShouldHost()) {
@@ -106,12 +114,27 @@ public class Main {
         log.info("Deployed to k8s with subdomain: " + autoCD.getSubdomain());
     }
 
+    /**
+     * If its a vue Project, it requires Port 80. Because its simpler to check for .vue than changing the nginx
+     * configuration, this method will change the container port from the default value 8080 to 80.
+     *
+     * @param autoCD
+     * @param finder
+     */
     private static void populateContainerPort(AutoCD autoCD, DockerfileHandler finder) {
         if (autoCD.getContainerPort() == 8080 && finder.getFileType().equals(FileType.VUE)) {
             autoCD.setContainerPort(80);
         }
     }
 
+    /**
+     * If there is no image on the registry, creates new Dockerfile, builds an images and pushes it to the registry
+     * and sets its path in the autoCD object.
+     *
+     * @param autoCD
+     * @param buildType
+     * @param finder
+     */
     private static void populateRegistryImagePath(AutoCD autoCD, String buildType, DockerfileHandler finder) {
         if (autoCD.getRegistryImagePath() == null || autoCD.getRegistryImagePath().isEmpty()) {
             var dockerFile = new File("Dockerfile");
@@ -126,6 +149,14 @@ public class Main {
         }
     }
 
+    /**
+     * If the autoCD object can not be found, this method creates a new autoCD object and returns it.
+     * If there is one, the autoCD object will get its parameters from an exiting configuration file.
+     *
+     * @param name
+     * @param createIfNotExists
+     * @return
+     */
     private static AutoCD getAutoCD(String name, boolean createIfNotExists) throws FileNotFoundException {
         var autocdConfigFile = new File(name);
         AutoCD autoCD;
@@ -138,6 +169,16 @@ public class Main {
         return autoCD;
     }
 
+    /**
+     * This method looks for subdomains within the autoCD object, if there are any, the subdomain will be the one with the
+     * matching build type.
+     * If autoCD does not have any subdomains, this method will build one out of the build type and an hashed version
+     * of the registry image path.
+     *
+     * @param autoCD
+     * @param buildType
+     * @param subdomains
+     */
     private static void populateSubdomain(AutoCD autoCD, String buildType, Map<String, String> subdomains) {
         if (autoCD.getSubdomains() != null && subdomains.keySet().size() != 0) {
             autoCD.setSubdomain(autoCD.getSubdomains().get(buildType));
@@ -148,6 +189,14 @@ public class Main {
         }
     }
 
+    /**
+     * If a service should be removed, this method checks for other services, depending on the one which will be removed from the
+     * cluster. Dependencies can be found in the autoCD class variable called otherImages. The method proceeds recursively
+     * and removes all depending services.
+     *
+     * @param autoCD
+     * @param k8sClient
+     */
     private static void removeWithDependencies(AutoCD autoCD, K8sClient k8sClient) {
         if (!autoCD.getOtherImages().isEmpty()) {
             autoCD.getOtherImages().forEach(config -> {
@@ -161,6 +210,12 @@ public class Main {
         k8sClient.removeDeploymentFromK8s(autoCD);
     }
 
+    /**
+     * Creates a name for a depending service out of the project name and a hashed registry image path from the main service
+     *
+     * @param main
+     * @param other
+     */
     private static void setServiceNameForOtherImages(AutoCD main, AutoCD other) {
         if (other.getServiceName() == null) {
             other.setServiceName(Util.hash(
@@ -169,6 +224,13 @@ public class Main {
         }
     }
 
+    /**
+     * If there are dependencies found within the main service, those services will be deployed as well.
+     *
+     * @param autoCD
+     * @param k8sClient
+     * @param buildType
+     */
     private static void deployWithDependencies(AutoCD autoCD, K8sClient k8sClient, String buildType) {
         validateConfig(autoCD);
         if (!autoCD.getOtherImages().isEmpty()) {
@@ -186,6 +248,11 @@ public class Main {
         k8sClient.deployToK8s(autoCD);
     }
 
+    /**
+     * Note that autoCD does not support retaining volumes if the replica set is greater than 1.
+     *
+     * @param autoCD
+     */
     private static void validateConfig(AutoCD autoCD) {
         if (autoCD.getReplicas() > 1) {
             if (autoCD.getVolumes().stream().anyMatch(Volume::isRetainVolume)) {
